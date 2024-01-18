a3181ca7c8d7345c6c4a4f36d673b7930a719fb1
mitjavaz <120010481@qq.com>
2024-01-18 22:08:30 +0800

    update

---
+++ .ready/_批量解压.bat
@@ -0,0 +1,3 @@
+for %%a in ("*.zip") do Bandizip.exe x -p:"www.javaxxz.com_&YHu6GJ(kjN" -o:.\ "%%a"
+for %%a in ("*.rar") do Bandizip.exe x -p:"www.javaxxz.com_&YHu6GJ(kjN" -o:.\ "%%a"
+for %%a in ("*.7z") do Bandizip.exe x -p:"www.javaxxz.com_&YHu6GJ(kjN" -o:.\ "%%a"
