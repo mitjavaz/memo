00d51b55bea585b02c48887aaeff55ead65594c6
mitjavaz <120010481@qq.com>
2024-04-29 10:35:52 +0800

    1

--- .ready/_批量解压.bat
+++ .ready/_批量解压.bat
@@ -1,3 +1,3 @@
-for %%a in ("*.zip") do Bandizip.exe x -p:"www.javaxxz.com_&YHu6GJ(kjN" -o:.\ "%%a"
-for %%a in ("*.rar") do Bandizip.exe x -p:"www.javaxxz.com_&YHu6GJ(kjN" -o:.\ "%%a"
-for %%a in ("*.7z") do Bandizip.exe x -p:"www.javaxxz.com_&YHu6GJ(kjN" -o:.\ "%%a"
+for %%a in ("*.zip") do Bandizip.exe x -p:"www.itjc8.com)A1Hw3XDEK!rH" -o:.\ "%%a"
+for %%a in ("*.rar") do Bandizip.exe x -p:"www.itjc8.com)A1Hw3XDEK!rH" -o:.\ "%%a"
+for %%a in ("*.7z") do Bandizip.exe x -p:"www.itjc8.com)A1Hw3XDEK!rH" -o:.\ "%%a"
